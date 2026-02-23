b1071d550b31118f1cf8e70a3269b1c44e94c620
Jobin Jose <jobin.jose@tresvance.com>
2026-02-23 22:55:16 +0530

    updates

---
+++ custom_addons/recruitment_custom/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Recruitment Custom',
+    'version': '17.0.1.0.0',
+    'category': 'Recruitment',
+    'summary': 'Add current salary to recruitment applicant',
+    'author': 'Abin',
+    'depends': ['hr_recruitment'],
+    'data': [
+        'views/hr_applicant_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
